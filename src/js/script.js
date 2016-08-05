588bbd6cdbcf4fe17a3e2c171cb2810fe2235e25
Alecsandr <alecsandrcherneckiy@gmail.com>
2016-08-05 08:25:38 +0300

    first

---
+++ src/js/script.js
@@ -0,0 +1,18 @@
+window.log = function(param){
+    console.log(param);
+};
+
+$(document).ready(function(){
+
+    try {
+        $.browserSelector();
+        if($("html").hasClass("chrome")) {
+            $.smoothScroll();
+        }
+    } catch(err) {
+
+    };
+
+    console.log('jQuery работает');
+
+});
